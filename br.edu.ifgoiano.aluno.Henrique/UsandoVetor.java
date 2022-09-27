43b460ccdddaadab155fc8bb04b8d8f8962a695e
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-27 13:12:00 -0300

    Alterando o Vetor para Multiplicar ele por ele mesmo.

--- br.edu.ifgoiano.aluno.Henrique/UsandoVetor.java
+++ br.edu.ifgoiano.aluno.Henrique/UsandoVetor.java
@@ -2,6 +2,13 @@ package br.edu.ifgoiano.aluno.Henrique;
 
 public class UsandoVetor {
     public static void main(String[] args) {
+        int [] vetor = new int [4];
 
+        for (int i = 0; i < 4; i++){
+            vetor[i] = i;
+            vetor[i] *= vetor[i];
+            System.out.println(vetor[i]);
+
+        }
     }
 }
