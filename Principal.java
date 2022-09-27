9ee82280e6aea047fba7f14ef0bc1a9e65303d28
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-27 12:41:07 -0300

    Chamando a Função dentro da Classe Principal

---
+++ Principal.java
@@ -0,0 +1,10 @@
+import br.edu.ifgoiano.funcao.FuncaoSoma;
+public class Principal {
+    public static void main(String[] args) {
+        FuncaoSoma chamar = new FuncaoSoma();
+
+        chamar.Somar(6,7,8);
+
+
+    }
+}
---
+++ br/edu/ifgoiano/funcao/FuncaoSoma.java
@@ -0,0 +1,7 @@
+package br.edu.ifgoiano.funcao;
+
+public class FuncaoSoma {
+    public int Somar(int a, int b, int c){
+        return a+b+c;
+    }
+}
