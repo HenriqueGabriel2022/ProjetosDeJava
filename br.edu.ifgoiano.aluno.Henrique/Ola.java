90b0bc3157f0ac04c88d272a27806cfa2d87b083
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-28 10:30:22 -0300

    Usando método dentro da classe principal e chamando o mesmo dentro do main principal.

---
+++ br.edu.ifgoiano.aluno.Henrique/Ola.java
@@ -0,0 +1,12 @@
+package br.edu.ifgoiano.aluno.Henrique;
+
+public class Ola {
+    public void OlaMundo(){
+        System.out.println("Olá Mundo");
+    }
+
+    public static void main(String[] args) {
+        Ola cumprimento = new Ola();
+        cumprimento.OlaMundo();
+    }
+}
