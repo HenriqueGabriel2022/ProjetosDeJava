e5a68a3021fdff33d73188e67e37240f2a34eb6d
Henrique Gabriel <75758371+Hgg2020-sudo@users.noreply.github.com>
2022-09-28 11:03:43 -0300

    Adicionando comemtário.

--- br.edu.ifgoiano.aluno.Henrique/Ola.java
+++ br.edu.ifgoiano.aluno.Henrique/Ola.java
@@ -2,7 +2,7 @@ package br.edu.ifgoiano.aluno.Henrique;
 
 public class Ola {
     public void OlaMundo(){
-        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo bonito");
     }
 
     public static void main(String[] args) {
